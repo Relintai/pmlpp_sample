4778fcca8470b48d313791e8fd9553e69bb96306
Relintai <relintai@protonmail.com>
2023-04-26 22:09:12 +0200

    Updated the engine.

--- game/test/TestMatrix.gd
+++ game/test/TestMatrix.gd
@@ -5,5 +5,9 @@ var mlpp_tests : MLPPMatrixTests = null
 func _ready() -> void:
 	mlpp_tests = MLPPMatrixTests.new()
 
-	mlpp_tests.test_mlpp_matrix()
-	mlpp_tests.test_mlpp_matrix_mul()
+	mlpp_tests.run_tests()
+	
+	#mlpp_tests.test_mlpp_matrix()
+	#mlpp_tests.test_mlpp_matrix_mul()
+	
+	get_tree().quit()
